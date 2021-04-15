760d906c6fada6d3cc6910e24cf4473fdf05cefe
Rawan.alakhras <ralakhras36@gmail.com>
2021-04-15 16:37:07 +0300

    the problem 6 has been solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -142,10 +142,16 @@ Test this function by hand in the console to get it working, and when you think 
 let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    let Cumulative = 1;
+    for (let i=0 ;i <dynamicArray.length ; i++){
+        Cumulative = multiply(Cumulative,dynamicArray[i])[0];
+    }
+    let msg ="The numbers "+dynamicArray+" have a product of "+Cumulative+".";
+    let result = [Cumulative,msg];
+    return result;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
